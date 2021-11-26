7d2acff338e401f081f7d7c9f8bb8a6760921c1a
Oleksandr Mozol <omozol@akamai.com>
2021-11-26 18:28:47 +0100

    minor fixes

--- demo-clients.sh
+++ demo-clients.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-echo "Start"
+red=$(tput setaf 5)
+green=$(tput setaf 2)
+normal=$(tput sgr0)
+SC_SUB="demo.sub"
+SB_SUB="demo.shop"
+
+
+function start_setup {
+printf "\n\n\n=================================================================\n\n\n"
+printf "${green}Start${normal}\n\n"
 
 CS_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' compose_cacheserve_1)
 PX_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' compose_proxy_1)
@@ -8,7 +17,7 @@ PX_IP_PREFIX=$(echo -n "$PX_IP" | sed -n -e 's/\([0-9]\+\.[0-9]\+\.[0-9]\+\)\.[0
 
 echo "Cacheserve IP is ${CS_IP}"
 echo "Proxy IP is ${PX_IP}"
-
+}
 
 function prepare_proxy {
 echo "Installing iproute package on nom-proxy"
@@ -25,7 +34,7 @@ done
 function update_proxy_routing {
 ROUTING="---
 global:
-routing:
+  routing:
     device-groups:
       - name: DEFAULT
         sites:
@@ -135,7 +144,8 @@ dc up -d wifi-dev mob-dev
 
 
 function configure_cpe {
-docker exec -i compose_cpe_1 sh -c "apk update && apk upgrade && apk add iptables"
+docker exec -i compose_cpe_1 sh -c "apk update && apk upgrade &>/dev/null"
+docker exec -i compose_cpe_1 sh -c "apk add iptables"
 docker exec -i compose_cpe_1 sh -c "ip route del default"
 docker exec -i compose_cpe_1 sh -c "ip route add default via ${PX_IP_PREFIX}.1"
 CPE_NET_IP_PREFIX=$(echo -n "$CPE_PRIV_IP" | sed -n -e 's/\([0-9]\+\.[0-9]\+\.[0-9]\+\)\.[0-9]\+/\1/p')
@@ -151,19 +161,20 @@ docker exec -i compose_wifi-dev_1 sh -c "ip route add ${YOUR_IP}/32 via ${CPE_NE
 }
 
 
-function configure_subscriber {
+function configure_subscriber_sc {
 SSM_PWD=$(docker exec -i compose_sportal_1 sh -c 'cat /usr/local/nom/etc/sportal/sportal_test_users.properties | head -1 | cut -d" " -f 4')
+WIFI_DEV_MAC=$(docker inspect compose_wifi-dev_1 | jq -r '.[] | .NetworkSettings.Networks["compose_cpe-1"]["MacAddress"]')
 MOB_DEV_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' compose_mob-dev_1)
 # Setup subscriber
-curl -H 'Content-type: application/json' -u admin:${SSM_PWD} --request POST --data '{"id":"demo.sub","time-zone":"UTC"}' http://localhost:9090/control
-curl -H 'Content-type: application/json' -u admin:${SSM_PWD} --request POST --data '{"service":"personal-internet","service-profile":"perdevice"}' http://localhost:9090/control/demo.sub/service
-curl -H 'Content-type: application/json' -u admin:${SSM_PWD} --request POST --data '{"service":"subscriber-safety","service-profile":"standard"}' http://localhost:9090/control/demo.sub/service
-curl -H 'Content-type: application/json' -u admin:${SSM_PWD} --request POST --data '{"add": [{"name": "demo.sub@wifi", "list": "fixed"},{"name": "3955555555", "list": "mobile"}]}' http://localhost:9090/account/demo.sub/line
+curl -H 'Content-type: application/json' -u admin:${SSM_PWD} --request POST --data "{\"id\":\"${SC_SUB}\",\"time-zone\":\"UTC\"}" http://localhost:9090/control
+curl -H 'Content-type: application/json' -u admin:${SSM_PWD} --request POST --data '{"service":"personal-internet","service-profile":"perdevice"}' http://localhost:9090/control/${SC_SUB}/service
+curl -H 'Content-type: application/json' -u admin:${SSM_PWD} --request POST --data '{"service":"subscriber-safety","service-profile":"standard"}' http://localhost:9090/control/${SC_SUB}/service
+curl -H 'Content-type: application/json' -u admin:${SSM_PWD} --request POST --data '{"add": [{"name": "demo.sub@wifi", "list": "fixed"},{"name": "3955555555", "list": "mobile"}]}' http://localhost:9090/account/${SC_SUB}/line
 # Setup view-selectors
-curl -H 'Content-type: application/json' -u admin:${SSM_PWD} --request POST --data "{\"view\":\"demo.sub\", \"source-address\":\"${MOB_DEV_IP}\", \"default-device-id\": \"3955555555\"}" http://localhost:8082/view-selector/
-curl -H 'Content-type: application/json' -u admin:${SSM_PWD} --request POST --data "{\"view\":\"demo.sub\", \"source-address\":\"${CPE_DEF_IP}\"}" http://localhost:8082/view-selector/
+curl -H 'Content-type: application/json' -u admin:${SSM_PWD} --request POST --data "{\"view\":\"${SC_SUB}\", \"source-address\":\"${MOB_DEV_IP}\", \"default-device-id\": \"3955555555\"}" http://localhost:8082/view-selector/
+curl -H 'Content-type: application/json' -u admin:${SSM_PWD} --request POST --data "{\"view\":\"${SC_SUB}\", \"source-address\":\"${CPE_DEF_IP}\"}" http://localhost:8082/view-selector/
 # Add pwd for sportal
-docker exec -i compose_sportal_1 bash -c 'echo "demo.sub = demo.sub demo ROLE_USER per-device" >>/usr/local/nom/etc/sportal/sportal_test_users.properties'
+docker exec -i compose_sportal_1 bash -c "echo \"${SC_SUB} = ${SC_SUB} demo ROLE_USER per-device\" >>/usr/local/nom/etc/sportal/sportal_test_users.properties"
 # Configure profiles
 cat >profiles.json <<EOF
 [
@@ -199,11 +210,11 @@ cat >profiles.json <<EOF
         "default": false,
         "internet-access": true,
         "protection-enabled": true,
-        "safe-search": true,
+        "safe-search": false,
         "safe-search-services": {
-            "youtube": true,
-            "google": true,
-            "bing": true
+            "youtube": false,
+            "google": false,
+            "bing": false
         },
         "name": "adults",
         "age-group": "pi-ag3",
@@ -228,28 +239,48 @@ cat >profiles.json <<EOF
 ]
 EOF
 
-WIFI_DEV_MAC=$(docker inspect compose_wifi-dev_1 | jq -r '.[] | .NetworkSettings.Networks["compose_cpe-1"]["MacAddress"]')
-
-curl -H 'Content-type: application/json' -u admin:${SSM_PWD} --request POST --data @profiles.json http://localhost:9090/pi/demo.sub/profile
-curl -H 'Content-type: application/json' -u admin:${SSM_PWD} --request POST --data "{\"add\":[{\"name\":\"Tablet\",\"profile\":\"kids\",\"identifiers\":[\"${WIFI_DEV_MAC}\"]}]}" http://localhost:9090/account/demo.sub/logical-device
-curl -H 'Content-type: application/json' -u admin:${SSM_PWD} --request POST --data "{\"add\":[{\"name\":\"Mobile\",\"profile\":\"adults\",\"identifiers\":[\"3955555555\"]}]}" http://localhost:9090/account/demo.sub/logical-device
+curl -H 'Content-type: application/json' -u admin:${SSM_PWD} --request POST --data @profiles.json http://localhost:9090/pi/${SC_SUB}/profile
+curl -H 'Content-type: application/json' -u admin:${SSM_PWD} --request POST --data "{\"add\":[{\"name\":\"Tablet\",\"profile\":\"kids\",\"identifiers\":[\"${WIFI_DEV_MAC}\"]}]}" http://localhost:9090/account/${SC_SUB}/logical-device
+curl -H 'Content-type: application/json' -u admin:${SSM_PWD} --request POST --data "{\"add\":[{\"name\":\"Mobile\",\"profile\":\"adults\",\"identifiers\":[\"3955555555\"]}]}" http://localhost:9090/account/${SC_SUB}/logical-device
 }
 
 function finish_setup {
 EXT_IP=$(curl --silent --header 'Metadata-Flavor: Google' http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
-printf "\n\n\n"
-echo "================================================================="
-echo "Setup completed"
+printf "\n\n\n=================================================================\n\n\n"
+printf "${green}Setup completed${normal}\n\n"
 echo "Mobile device web-browser available at http://${EXT_IP}:8085"
 echo "Home wi-fi device web-browser available at http://${EXT_IP}:8083"
-echo "Subscriber account management via portal is available at http://${EXT_IP}:8444, login: demo.sub, pwd: demo"
+echo "Subscriber account management via portal is available at http://${EXT_IP}:8444, login: ${SC_SUB}, pwd: demo"
 }
 
-prepare_proxy
-update_proxy_routing
-deploy_cpe
-deploy_clients
-configure_cpe
-confiugure_wifi_dev
-configure_subscriber
-finish_setup
+function stop_services {
+sed -i '/CPE START/,$d' /usr/local/nom/share/nom-demo/docker/compose/docker-compose.yml
+dc up -d --remove-orphans
+}
+
+function remove_subscriber {
+subscriber=$1
+SSM_PWD=$(docker exec -i compose_sportal_1 sh -c 'cat /usr/local/nom/etc/sportal/sportal_test_users.properties | head -1 | cut -d" " -f 4')
+curl -H 'Content-type: application/json' -u admin:${SSM_PWD} --request POST --data '{"metrics": ["content", "safety", "botnet", "dns", "device-discovery"]}' http://localhost:9090/account/${subscriber}/history:force-clear
+curl -u admin:${SSM_PWD} --request DELETE http://localhost:9090/account/${subscriber}
+}
+
+
+if [[ $1 == start_sc ]]
+then
+  start_setup
+  prepare_proxy
+  update_proxy_routing
+  deploy_cpe
+  deploy_clients
+  configure_cpe
+  confiugure_wifi_dev
+  configure_subscriber_sc
+  finish_setup
+fi
+
+if [[ $1 == stop_sc ]]
+then
+  stop_services
+  remove_subscriber ${SC_SUB}
+fi
